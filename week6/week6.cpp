1ad848645d7bf113a0fd0212778cfa12f6cd1e14
桐 孙 <2731881741@qq.com>
2020-04-03 14:19:25 +0800

    练习4

--- week6/week6.cpp
+++ week6/week6.cpp
@@ -7,7 +7,31 @@ using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat srcMat = imread("C:\\Users\\27318\\Desktop\\大二下网络课程\\数字图像\\第六周\\lena.jpg", 1);
+	cv::Mat dstMat;
+	if (srcMat.empty()) return -1;
+
+	//变换前的四点坐标
+	cv::Point2f pts1[] = {
+		cv::Point2f(150,150),
+		cv::Point2f(150,300),
+		cv::Point2f(350,300),
+		cv::Point2f(250,100)
+	};
+	//变换后的四点坐标
+	cv::Point2f pts2[] = {
+		cv::Point2f(200,150),
+		cv::Point2f(200,300),
+		cv::Point2f(340,270),
+		cv::Point2f(340,180)
+	};
+	//计算仿射矩阵
+	cv::Mat perspective_matrix = cv::getPerspectiveTransform(pts1, pts2);
+
+	cv::warpPerspective(srcMat, dstMat, perspective_matrix, srcMat.size());
+	cv::imshow("srcMat", srcMat);
+	cv::imshow("dstMat", dstMat);
+	waitKey(0);
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
