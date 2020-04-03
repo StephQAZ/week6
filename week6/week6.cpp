95d6d786c884fd37bab68a0464ce257e5f35705d
桐 孙 <2731881741@qq.com>
2020-04-03 13:59:28 +0800

    练习1

--- week6/week6.cpp
+++ week6/week6.cpp
@@ -7,7 +7,19 @@ using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat srcMat = imread("C:\\Users\\27318\\Desktop\\大二下网络课程\\数字图像\\第六周\\lena.jpg");
+	cv::Mat dstMat,dstMat1,dx,dy;
+
+	int threshold = 20;
+	Sobel(srcMat, dx, CV_16S, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+	Sobel(srcMat, dy, CV_16S, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+	Canny(dx, dy, dstMat, threshold, threshold*3);
+	imshow("lena1", dstMat);
+
+	Canny(srcMat, dstMat1, threshold, threshold * 3);
+	imshow("lena2", dstMat1);
+
+	waitKey(0);
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
