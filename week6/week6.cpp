2c9a6aa341f1776b173a09d34721a9d7aad6abd8
桐 孙 <2731881741@qq.com>
2020-04-03 14:43:55 +0800

    练习5

--- week6/week6.cpp
+++ week6/week6.cpp
@@ -4,10 +4,68 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 using namespace cv;
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat srcMat = imread("C:\\Users\\27318\\Desktop\\大二下网络课程\\数字图像\\第六周\\lena_rot.jpg", 1);
+	cv::Mat grayMat;
+	cv::Mat dstMat;
+	cvtColor(srcMat, grayMat, CV_BGR2GRAY);
+	if (srcMat.empty()) return -1;
+
+	int height = grayMat.rows;
+	int width = grayMat.cols;
+	int srcX_1,srcY_1, srcX_2, srcY_2, srcX_3, srcY_3, srcX_4, srcY_4 = 0;
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+			if (j == 0 && grayMat.at<uchar>(j, i) < 250) {
+				srcX_1 = j;
+				srcY_1 = i;
+			}
+			if (i == width - 1 && grayMat.at<uchar>(j, i) < 250) {
+				srcX_2 = j;
+				srcY_2 = i;
+			}
+			if (i == 0 && grayMat.at<uchar>(j, i) < 250) {
+				srcX_3 = j;
+				srcY_3 = i;
+			}
+			if (i == 0 && grayMat.at<uchar>(j, i) < 250) {
+				srcX_3 = j;
+				srcY_3 = i;
+			}
+			if (j == height - 1 && grayMat.at<uchar>(j, i) < 250) {
+				srcX_4 = j;
+				srcY_4 = i;
+			}
+		}
+	}
+	cout << srcX_1 << " , " << srcY_1 << endl;
+	cout << srcX_2 << " , " << srcY_2 << endl;
+	cout << srcX_3 << " , " << srcY_3 << endl;
+	cout << srcX_4 << " , " << srcY_4 << endl;
+	cout << height << " , " << width << endl;
+	//变换前的四点坐标
+	cv::Point2f pts1[] = {
+		cv::Point2f(srcY_1,srcX_1),
+		cv::Point2f(srcY_2,srcX_2),
+		cv::Point2f(srcY_3,srcX_3),
+		cv::Point2f(srcY_4,srcX_4)
+	};
+	//变换后的四点坐标
+	cv::Point2f pts2[] = {
+		cv::Point2f(0,0),
+		cv::Point2f(width - 1,0),
+		cv::Point2f(0,height - 1),
+		cv::Point2f(width - 1,height - 1)
+	};
+	//计算仿射矩阵
+	cv::Mat perspective_matrix = cv::getPerspectiveTransform(pts1, pts2);
+
+	cv::warpPerspective(srcMat, dstMat, perspective_matrix, srcMat.size());
+	cv::imshow("srcMat", srcMat);
+	cv::imshow("dstMat", dstMat);
+	waitKey(0);
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
